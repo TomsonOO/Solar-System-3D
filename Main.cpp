48d1b37c3bb9703f24bb92e183bb806e07bbd784
Tomson <tomasz.rzepka52@gmail.com>
2022-06-12 16:06:03 +0200

    Upgrade from 2D to 3D

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,10 @@
-﻿#define _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
 
+#define GL_SILENCE_DEPRECATION
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
 #include <GLFW/glfw3.h>
-#include <glad/glad.h>
 #include <iostream>
 #include <cmath>
 #include <time.h>
@@ -10,25 +13,21 @@
 #include <time.h>
 #include <stdio.h>
 
-#define SCREEN_WIDTH 1600
-#define SCREEN_HEIGHT 1600
-#define START_X 440
-#define START_Y 880
-#define z 0.0f
+
 #define segment 360
-
-#define Sun_radius 40
-#define Earth_radius 9
-
-#define Mercury_radius 4
-#define Venus_radius 10
-#define Mars_radius 12
-#define Jupiter_radius 18
-#define Saturn_radius 15
-#define Neptun_radius 13
-
+#define Sun_radius 0.4
+#define Earth_radius 0.12
+#define Mercury_radius 0.08
+#define Venus_radius 0.11
+#define Mars_radius 0.1
+#define Jupiter_radius 0.25
+#define Saturn_radius 0.2
+#define Neptun_radius 0.18
+#define alpha 1.0f
 
 using namespace std;
+
+void timer(int);
 
 class Planet {
 public:
@@ -42,221 +41,114 @@ public:
         b = B;
     }
 
-    void DrawOrbit() {
+    void DrawSun(GLfloat x, GLfloat y, GLfloat z) {
 
-        GLfloat x = START_X;
-        GLfloat y = START_Y;
+        glEnable(GL_COLOR_TABLE);
+        glColor4f(r, g, b, alpha);
 
-        GLint vertices = segment + 2;
-        GLfloat DoublePi = 2.0f * M_PI;
+        glPushMatrix();
+        glTranslatef(x, y, z);
+        glutSolidSphere(radius, 30, 36);
+        glPopMatrix();
 
-        GLfloat circleX[segment + 2];
-        GLfloat circleY[segment + 2];
-        GLfloat circleZ[segment + 2];
-
-        for (int i = 0; i < vertices; i++) {
-            circleX[i] = x + (orbit * cos(i * DoublePi / segment));
-            circleY[i] = y + (orbit * sin(i * DoublePi / segment));
-            circleZ[i] = z;
-        }
-
-        GLfloat AllCircleVertices[(segment + 2) * 3];
-
-        for (int i = 0; i < vertices; i++) {
-            AllCircleVertices[i * 3] = circleX[i];
-            AllCircleVertices[(i * 3) + 1] = circleY[i];
-            AllCircleVertices[(i * 3) + 2] = circleZ[i];
-        }
-
-        glEnableClientState(GL_VERTEX_ARRAY);
-
-        glColor3f(1.0f, 1.0f, 1.0f);
-
-        glVertexPointer(3, GL_FLOAT, 0, AllCircleVertices);
-        glDrawArrays(GL_LINE_STRIP, 0, vertices);
-
-        glDisableClientState(GL_VERTEX_ARRAY);
+        glDisable(GL_COLOR_TABLE);
     }
 
-    void DrawPlanet(GLfloat x, GLfloat y) {
+    void DrawPlanet(GLfloat x, GLfloat y, GLfloat z) {
 
-        GLint vertices = segment + 2;
         GLfloat DoublePi = 2.0f * M_PI;
 
-        GLfloat circleX[segment + 2];
-        GLfloat circleY[segment + 2];
-        GLfloat circleZ[segment + 2];
+        GLfloat circleXYZ[3];
+        circleXYZ[0] = 0;
+        circleXYZ[1] = 0;
+        circleXYZ[2] = 0;
 
-        circleX[0] = x;
-        circleY[0] = y;
-        circleZ[0] = z;
-
-        for (int i = 1; i < vertices; i++) {
-            circleX[i] = x + (radius * cos(i * DoublePi / segment));
-            circleY[i] = y + (radius * sin(i * DoublePi / segment));
-            circleZ[i] = z;
+        glBegin(GL_LINES); // orbit drawing
+        for (float i = 0.0f; i < DoublePi; i += 0.01) {
+            float x1 = circleXYZ[0] + (cos(i) * orbit);
+            float y1 = circleXYZ[1];
+            float z1 = circleXYZ[2] + (sin(i) * orbit);
+            glColor4f(1.0, 1.0, 1.0, alpha);
+            glVertex3f(x1, y1, z1);
         }
+        glEnd();
 
-        GLfloat AllCircleVertices[(segment + 2) * 3];
+        glEnable(GL_COLOR_TABLE);
 
-        for (int i = 0; i < vertices; i++) {
-            AllCircleVertices[i * 3] = circleX[i];
-            AllCircleVertices[(i * 3) + 1] = circleY[i];
-            AllCircleVertices[(i * 3) + 2] = circleZ[i];
-        }
+        glColor4f(r, g, b, alpha);
 
-        glEnableClientState(GL_VERTEX_ARRAY);
+        glPushMatrix();
+        glTranslatef(x, y, z);
+        glutSolidSphere(radius, 30, 36);
+        glPopMatrix();
 
-        glColor3f(r, g, b);
-
-        glVertexPointer(3, GL_FLOAT, 0, AllCircleVertices);
-        glDrawArrays(GL_TRIANGLE_FAN, 0, vertices);
-
-        glDisableClientState(GL_VERTEX_ARRAY);
+        glDisable(GL_COLOR_TABLE);
     }
 
-    void Next_step_Draw(GLfloat period, GLfloat time)
-    {
+    void PlanetAnimation(GLfloat period, GLfloat time) {
         GLfloat w = (2.0f * M_PI) / period;
-        GLfloat x = START_X + orbit * cos(w * time);
-        GLfloat y = START_Y + orbit * sin(w * time);
-        DrawPlanet(x, y);
+        GLfloat x = orbit * cos(w * time);
+        GLfloat y = 0.0f;
+        GLfloat z = orbit * sin(w * time);
+        DrawPlanet(x, y, z);
     }
-
-    void DrawOrbitMoon(GLfloat x, GLfloat y) {
-
-        GLint vertices = segment + 2;
-        GLfloat DoublePi = 2.0f * M_PI;
-
-        GLfloat circleX[segment + 2];
-        GLfloat circleY[segment + 2];
-        GLfloat circleZ[segment + 2];
-
-        for (int i = 0; i < vertices; i++) {
-            circleX[i] = x + (orbit * cos(i * DoublePi / segment));
-            circleY[i] = y + (orbit * sin(i * DoublePi / segment));
-            circleZ[i] = z;
-        }
-
-        GLfloat AllCircleVertices[(segment + 2) * 3];
-
-        for (int i = 0; i < vertices; i++) {
-            AllCircleVertices[i * 3] = circleX[i];
-            AllCircleVertices[(i * 3) + 1] = circleY[i];
-            AllCircleVertices[(i * 3) + 2] = circleZ[i];
-        }
-
-        glEnableClientState(GL_VERTEX_ARRAY);
-
-        glColor3f(1.0f, 1.0f, 1.0f);
-
-        glVertexPointer(3, GL_FLOAT, 0, AllCircleVertices);
-        glDrawArrays(GL_LINE_STRIP, 0, vertices);
-
-        glDisableClientState(GL_VERTEX_ARRAY);
-    }
-
-    void DrawPlanetMoob(GLfloat x, GLfloat y) {
-
-        GLint vertices = segment + 2;
-        GLfloat DoublePi = 2.0f * M_PI;
-
-        GLfloat circleX[segment + 2];
-        GLfloat circleY[segment + 2];
-        GLfloat circleZ[segment + 2];
-
-        circleX[0] = x;
-        circleY[0] = y;
-        circleZ[0] = z;
-
-        for (int i = 1; i < vertices; i++) {
-            circleX[i] = x + (radius * cos(i * DoublePi / segment));
-            circleY[i] = y + (radius * sin(i * DoublePi / segment));
-            circleZ[i] = z;
-        }
-
-        GLfloat AllCircleVertices[(segment + 2) * 3];
-
-        for (int i = 0; i < vertices; i++) {
-            AllCircleVertices[i * 3] = circleX[i];
-            AllCircleVertices[(i * 3) + 1] = circleY[i];
-            AllCircleVertices[(i * 3) + 2] = circleZ[i];
-        }
-
-        glEnableClientState(GL_VERTEX_ARRAY);
-
-        glColor3f(r, g, b);
-
-        glVertexPointer(3, GL_FLOAT, 0, AllCircleVertices);
-        glDrawArrays(GL_TRIANGLE_FAN, 0, vertices);
-
-        glDisableClientState(GL_VERTEX_ARRAY);
-    }
-
-
+    
 };
 
 Planet Sun(Sun_radius, 0, 1.0f, 0.8f, 0.0f);
-Planet Mercury(Mercury_radius, 60, 1.0f, 0.0f, 0.0f);
-Planet Venus(Venus_radius, 100, 0.0f, 0.0f, 1.0f);
-Planet Earth(Earth_radius, 130, 0.0f, 1.0f, 0.0f);
-Planet Mars(Mars_radius, 160, 1.0f, 0.5f, 0.0f);
-Planet Jupiter(Jupiter_radius, 220, 1.0f, 0.7f, 0.2f);
-Planet Saturn(Saturn_radius, 260, 1.0f, 0.0f, 0.2f);
-Planet Neptune(Neptun_radius, 290, 0.0f, 0.8f, 1.0f);
+Planet Mercury(Mercury_radius, 0.9, 1.0f, 0.0f, 0.0f);
+Planet Venus(Venus_radius, 1.2, 0.0f, 0.0f, 1.0f);
+Planet Earth(Earth_radius, 1.45, 0.0f, 1.0f, 0.0f);
+Planet Mars(Mars_radius, 1.7, 1.0f, 0.5f, 0.0f);
+Planet Jupiter(Jupiter_radius, 2.2, 1.0f, 0.7f, 0.2f);
+Planet Saturn(Saturn_radius, 2.7, 1.0f, 0.0f, 0.2f);
+Planet Neptune(Neptun_radius, 3.2, 0.0f, 0.8f, 1.0f);
 
+void init(void){
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glShadeModel(GL_FLAT);
+}
 
-int main() {
+void SolarSystem(void) // render function
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    GLfloat time = static_cast<GLfloat>(glutGet(GLUT_ELAPSED_TIME)) / 1000;
+    
+    Sun.DrawSun(0.0, 0.0f, 0.0f);
+    Mercury.PlanetAnimation(8.8f, time);
+    Venus.PlanetAnimation(22.465f, time);
+    Earth.PlanetAnimation(36.5, time);
+    Mars.PlanetAnimation(68.7, time);
+    Jupiter.PlanetAnimation(433, time);
+    Saturn.PlanetAnimation(1075.9, time);
+    Neptune.PlanetAnimation(6015.2, time);
+    glutPostRedisplay();
 
-    glfwInit();
+    glutSwapBuffers();
+}
 
-    GLFWwindow* window = glfwCreateWindow(1200, 1200, "Uklad sloneczny", NULL, NULL);
-    // Error check if the window fails to create
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" <<
-            std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    // Introduce the window into the current context
-    glfwMakeContextCurrent(window);
-
-    //Load GLAD so it configures OpenGL
-    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-    // Specify the viewport of OpenGL in the Window
-    // In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+void reshape(int w, int h){
     glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0.0f, 1200, 1200, 0.0f, 0.0f, 1.0f); // essentially set coordinate system
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+    gluPerspective(50.0, w / (GLfloat)h, 3.0, 90.0);
+    gluLookAt(2.0, 4.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
-    while (!glfwWindowShouldClose(window)) {
+}
 
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        Sun.DrawPlanet(440, 880);
-        Mercury.DrawOrbit();
-        Mercury.DrawPlanet(500, 880);
-        Venus.DrawOrbit();
-        Venus.DrawPlanet(540, 880);
-        Earth.DrawOrbit();
-        Earth.DrawPlanet(570, 880);
-        Mars.DrawOrbit();
-        Mars.DrawPlanet(600, 880);
-        Jupiter.DrawOrbit();
-        Jupiter.DrawPlanet(660, 880);
-        Saturn.DrawOrbit();
-        Saturn.DrawPlanet(700, 880);
-        Neptune.DrawOrbit();
-        Neptune.DrawPlanet(730, 880);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
+int main(int argc, char** argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(10, 10);
+    glutCreateWindow("SolarSystem");
+    
+    init();
+    glutDisplayFunc(SolarSystem);
+    glutReshapeFunc(reshape);
+    glutMainLoop();
     return 0;
+}
+
+void timer(int){
+    glutPostRedisplay();
+    glutTimerFunc(1000 / 60, timer, 0);
 }
